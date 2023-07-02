ef161ff61907861b06c4d6806f3b7312ef14c244
Noahns1 <noahschlaupitz@yahoo.com>
2023-07-01 22:35:44 -0400

    Completed Gherkin script for second test case

--- features/amazon.feature
+++ features/amazon.feature
@@ -4,7 +4,8 @@ Feature: Amazon Basics
     Given User opens up Chrome
     Then User goes to Amazon
 
-  Scenario: Amazon Basics - Fitness & Sporting Goods - Dumbbell in cart
+  @positive
+  Scenario: Amazon - Find Selenium book and add to cart
     When User clicks on search bar
     And User searches for selenium book
     Then Selects selenium book
@@ -12,4 +13,20 @@ Feature: Amazon Basics
     When User clicks on Add to Cart
     And User navigates to cart
     And User Verifies selenium book is in cart
+    Then close browser
+
+  @negative
+  Scenario: incorrectly attempt to create an account
+    When User clicks Account & Lists
+    Then User is on Sign in page
+    When User clicks Create your Amazon account
+    Then User is on Create account page
+    And User clicks continue
+    Then Verifies error messages from incorrect entries
+    When User enters invalid email
+    Then Verifies error message for email
+    When User enter mismatching passwords
+    Then Verifies error message for mismatching password
+    When User enters an improper password
+    Then Verifies error message for improper password
     Then close browser
--- features/steps/step_amazon.py
+++ features/steps/step_amazon.py
@@ -12,6 +12,8 @@ def step_open_chrome(context):
 def step_open_amazon(context):
     context.driver.get("https://www.amazon.com/")
 
+# Below scripts are for scenario: Amazon - Find Selenium book and add to cart
+
 @when('User clicks on search bar')
 def step_search_bar(context):
     context.driver.find_element(By.ID, 'twotabsearchtextbox').click()
@@ -57,3 +59,64 @@ def step_impl(context):
     context.driver.find_element(By.LINK_TEXT, 'Hands-On Selenium WebDriver '
                                               'with Java: A Deep Dive into the '
                                               'Development of End-to-End Tests')
+
+# Below scripts are for scenario: incorrectly attempt to create an account
+
+@when(u'User clicks Account & Lists')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User clicks Account & Lists')
+
+
+@then(u'User is on Sign in page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then User is on Sign in page')
+
+
+@when(u'User clicks Create your Amazon account')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User clicks Create your Amazon account')
+
+
+@then(u'User is on Create account page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then User is on Create account page')
+
+
+@then(u'User clicks continue')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then User clicks continue')
+
+
+@then(u'Verifies error messages from incorrect entries')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then Verifies error messages from incorrect entries')
+
+
+@when(u'User enters invalid email')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User enters invalid email')
+
+
+@then(u'Verifies error message for email')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then Verifies error message for email')
+
+
+@when(u'User enter mismatching passwords')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User enter mismatching passwords')
+
+
+@then(u'Verifies error message for mismatching password')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then Verifies error message for mismatching password')
+
+
+@when(u'User enters an improper password')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When User enters an improper password')
+
+
+@then(u'Verifies error message for improper password')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then Verifies error message for improper password')
